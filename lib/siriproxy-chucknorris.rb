3dba0a7fb9738dbd7b2bb7ca8d7d93a0c4856abc
robbederks <knutselman@gmail.com>
2012-03-31 16:49:57 +0300

    Added 'typo'-speech :D

--- lib/siriproxy-chucknorris.rb
+++ lib/siriproxy-chucknorris.rb
@@ -18,5 +18,13 @@ class SiriProxy::Plugin::GPP < SiriProxy::Plugin
 	say "#{@joke}"
 	request_completed
   end
+  listen_for /chuck noris joke/i do
+    say "Here is your Chuck Norris joke:"
+	@response = Net::HTTP.get(URI.parse("http://api.icndb.com/jokes/random"))
+	@result = JSON.parse(@response)
+	@joke = @result["value"]["joke"]
+	say "#{@joke}"
+	request_completed
+  end
  
 end
